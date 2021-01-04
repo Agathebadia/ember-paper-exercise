a49d8b7a0698aced4b800fd7b512ab0c7ba62352
Agathe Badia <ab@honeypot.io>
2021-01-04 13:31:41 +0100

    Add an adapter and a model for gif

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,6 @@
-import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import RESTAdapter from '@ember-data/adapter/rest';
 
-export default class ApplicationAdapter extends JSONAPIAdapter {
+export default class ApplicationAdapter extends RESTAdapter {
+  namespace = 'v1/gifs/random';
+  host = 'api.giphy.com';
 }
